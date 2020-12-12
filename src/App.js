b94d542deb89f5d0ecf2a8b1d86d2bab1d1d77f8
gsangayyahiremath <gsangayyahiremath@cain.com>
2020-12-12 19:53:49 +0530

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './assets/farm.jpg';
 
 function App() {
+  const { availHeight ,availWidth} = window.screen;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={image} alt={'Background'} className="backgroundImage" style={{
+        width:availWidth,
+        height:availHeight,
+      }}></img>
     </div>
   );
 }
